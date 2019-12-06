c8b7e1471492165a6e022fc7831823f34cb8d6e2
Dani Roman <roman.martinez.daniel@gmail.com>
2019-12-06 17:32:02 +0100

    Video 5: Add css style

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'; // Import React
 import  { render } from 'react-dom'; // Import render function from react-dom
 import StorePicker from './components/StorePicker';
+import "./css/style.css";
 
 render(<StorePicker></StorePicker>, document.querySelector('#main'));
