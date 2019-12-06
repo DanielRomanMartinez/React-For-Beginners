7f7fbf034494209a80ce3d5da235b4e8817c05bd
Dani Roman <roman.martinez.daniel@gmail.com>
2019-12-06 19:53:37 +0100

    Video 7: Passing Dynamic data with props

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
-                    <Header></Header>
+                    <Header tagline={"Fresh Dailly"}></Header>
                 </div>
                 <Inventory></Inventory>
                 <Order></Order>
