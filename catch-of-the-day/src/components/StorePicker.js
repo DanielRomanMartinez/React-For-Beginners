748ecb694426f3a240f82cc30886a7e83f2136fb
Dani Roman <roman.martinez.daniel@gmail.com>
2019-12-06 15:49:50 +0100

    Video 3 - Creating our First Components

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,10 @@
+import React from 'react'; // Import React. We can import React all the times that we want
+
+class StorePicker extends React.Component {
+
+    render(){
+        return <p>Hello</p>
+    }
+}
+
+export default StorePicker; // Export component
