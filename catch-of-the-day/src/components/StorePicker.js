e077e5556cc57a8532004577c85d11ff6af02144
Dani Roman <roman.martinez.daniel@gmail.com>
2019-12-06 16:18:43 +0100

    Video 4 - Example 2: Return without React.Fragment and add submit button

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -3,16 +3,12 @@ import React from 'react'; // Import React. We can import React all the times th
 class StorePicker extends React.Component {
 
     render(){
-        // return React.createElement('p', {className: 'hey'}, 'Heyooo'); // Otra forma de añadir elementos
-        // return <form action={""} className={"store-selector"}></form>;
         return (
-            <React.Fragment>
-                {/* Example comment */}
-                <p>Fish!</p>
-                <form action={""} className={"store-selector"}>
-                    <h1>Please Enter A Store</h1>
-                </form>
-            </React.Fragment>
+            <form action={""} className={"store-selector"}>
+                <h2>Please Enter A Store</h2>
+                <input type={"text"} required placeholder={"Store name"} />
+                <button type={"submit"}>Visit store</button>
+            </form>
         )
     }
 }
