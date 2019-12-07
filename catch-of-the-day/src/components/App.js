81036bb32c7fead9630f2c841f1346a3e72a818c
Dani Roman <roman.martinez.daniel@gmail.com>
2019-12-07 10:03:02 +0100

    Video 13: Understanding State

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,16 +4,30 @@ import Inventory from './Inventory';
 import Order from './Order';
 
 class App extends React.Component {
+    state = {
+        fishes: {},
+        order: {},
+    };
+    addFish = (fish) => {
+        // 1. Take a copy of the existing state
+        const fishes = {...this.state.fishes};
+
+        // 2. Add our new fish to that fishes variable
+        fishes[`fish${Date.now()}`] = fish;
+
+        // 3. Set the new fishes object to state
+        this.setState({ fishes });
+    };
     render() {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline={"Fresh Dailly"}></Header>
                 </div>
-                <Inventory></Inventory>
                 <Order></Order>
+                <Inventory addFish={this.addFish}></Inventory>
             </div>
         );
-    }
+    };
 }
 export default App;
